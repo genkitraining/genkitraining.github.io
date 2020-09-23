d7d7efab072aa6dd57f728602f12c92fd5083a0d
Genki Training <68614177+genkitraining@users.noreply.github.com>
2020-09-23 21:37:46 +0200

    Hoofdstuk 9 toegevoegd

---
+++ genki_ht9.js
@@ -0,0 +1,122 @@
+set_state("genki_ht9", [
+	{
+		"nederlands": "middag",
+		"francais": "midi",
+		"english": "noon",
+		"romaji": "go",
+		"hiragana": "ご",
+		"kanji": "午"
+	},
+	{
+		"nederlands": "na",
+		"francais": "après",
+		"english": "after",
+		"romaji": ["go", "ato/ushi"],
+		"hiragana": ["ご", "あと/うし"],
+		"kanji": "後"
+	},
+	{
+		"nederlands": "(er)voor",
+		"francais": "avant",
+		"english": "before",
+		"romaji": ["zen", "mae"],
+		"hiragana": ["ぜん", "まえ"],
+		"kanji": "前"
+	},
+	{
+		"nederlands": "naam",
+		"francais": "nom",
+		"english": "name",
+		"romaji": ["mei", "na"],
+		"hiragana": ["めい", "な"],
+		"kanji": "名"
+	},
+	{
+		"nederlands": "wit",
+		"francais": "blanc",
+		"english": "white",
+		"romaji": ["haku", "shiro"],
+		"hiragana": ["はく", "しろ"],
+		"kanji": "白"
+	},
+	{
+		"nederlands": "regen",
+		"francais": "pluie",
+		"english": "rain",
+		"romaji": ["u", "ame"],
+		"hiragana": ["う", "あめ"],
+		"kanji": "雨"
+	},
+	{
+		"nederlands": "schrijven",
+		"francais": "écrire",
+		"english": "to write",
+		"romaji": ["sho", "ka"],
+		"hiragana": ["しょ", "か"],
+		"kanji": "書"
+	},
+	{
+		"nederlands": "vriend",
+		"francais": "ami",
+		"english": "friend",
+		"romaji": ["yuu", "tomo"],
+		"hiragana": ["ゆう", "とも"],
+		"kanji": "友"
+	},
+	{
+		"nederlands": "tussen",
+		"francais": "entre",
+		"english": "between",
+		"romaji": ["kan/gen", "aida"],
+		"hiragana": ["かん/げん", "あいだ"],
+		"kanji": "間"
+	},
+	{
+		"nederlands": "huis",
+		"francais": "maison",
+		"english": "house",
+		"romaji": ["ka", "ie"],
+		"hiragana": ["か", "いえ"],
+		"kanji": "家"
+	},
+	{
+		"nederlands": "praten",
+		"francais": "parler",
+		"english": "to speak",
+		"romaji": ["wa", "hana/hanashi"],
+		"hiragana": ["わ", "はな/はなし"],
+		"kanji": "話"
+	},
+	{
+		"nederlands": "weinig",
+		"francais": "peu",
+		"english": "little",
+		"romaji": ["shou", "suko/suku"],
+		"hiragana": ["しょう", "すこ/すく"],
+		"kanji": "少"
+	},
+	{
+		"nederlands": "oud",
+		"francais": "vieux",
+		"english": "old",
+		"romaji": ["ko", "furu"],
+		"hiragana": ["こ", "ふる"],
+		"kanji": "古"
+	},
+	{
+		"nederlands": "kennen",
+		"francais": "connaître",
+		"english": "to know",
+		"romaji": ["chi", "shi"],
+		"hiragana": ["ち", "し"],
+		"kanji": "知"
+	},
+	{
+		"nederlands": "komen",
+		"francais": "venir",
+		"english": "to come",
+		"romaji": ["rai", "ku/ki/ko"],
+		"hiragana": ["らい", "く/き/こ"],
+		"kanji": "来"
+	}
+]);
