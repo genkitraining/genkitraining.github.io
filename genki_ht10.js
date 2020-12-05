f7746de913f7edd83eb14aaddc8e932b1552df5e
Genki Training <68614177+genkitraining@users.noreply.github.com>
2020-12-05 23:33:46 +0100

    Hoofdstuk 10

---
+++ genki_ht10.js
@@ -0,0 +1,114 @@
+set_state("genki_ht10", [
+	{
+		"nederlands": "leven (werkwoord)",
+		"francais": "vivre",
+		"english": "to live",
+		"romaji": ["juu", "su"],
+		"hiragana": ["じゅう", "す"],
+		"kanji": "住"
+	},
+	{
+		"nederlands": "correct",
+		"francais": "juste",
+		"english": "right",
+		"romaji": ["shou", "tada"],
+		"hiragana": ["しょう", "ただ"],
+		"kanji": "正"
+	},
+	{
+		"nederlands": "jaar",
+		"francais": "an",
+		"english": "year",
+		"romaji": ["nen", "toshi"],
+		"hiragana": ["ねん", "とし"],
+		"kanji": "年"
+	},
+	{
+		"nederlands": "verkopen",
+		"francais": "vendre",
+		"english": "to sell",
+		"romaji": ["bai", "u"],
+		"hiragana": ["ばい", "う"],
+		"kanji": "売"
+	},
+	{
+		"nederlands": "kopen",
+		"francais": "achèter",
+		"english": "to buy",
+		"romaji": ["bai", "ka"],
+		"hiragana": ["ばい", "か"],
+		"kanji": "買"
+	},
+	{
+		"nederlands": "dorp",
+		"francais": "ville",
+		"english": "town",
+		"romaji": ["chou", "machi"],
+		"hiragana": ["ちょう", "まち"],
+		"kanji": "町"
+	},
+	{
+		"nederlands": "lang",
+		"francais": "longue",
+		"english": "long",
+		"romaji": ["chou", "naga"],
+		"hiragana": ["ちょう", "なが"],
+		"kanji": "長"
+	},
+	{
+		"nederlands": "een weg",
+		"francais": "un chemin",
+		"english": "way",
+		"romaji": ["dou", "michi"],
+		"hiragana": ["どう", "みち"],
+		"kanji": "道"
+	},
+	{
+		"nederlands": "sneeuw",
+		"francais": "neige",
+		"english": "snow",
+		"romaji": ["setsu", "yuki"],
+		"hiragana": ["せつ", "ゆき"],
+		"kanji": "雪"
+	},
+	{
+		"nederlands": "staan",
+		"francais": "se tenir debout",
+		"english": "to stand",
+		"romaji": ["ritsu", "ta"],
+		"hiragana": ["りつ", "た"],
+		"kanji": "立"
+	},
+	{
+		"nederlands": "zelf",
+		"francais": "même",
+		"english": "self",
+		"romaji": "ji",
+		"hiragana": "じ",
+		"kanji": "自"
+	},
+	{
+		"nederlands": "nacht",
+		"francais": "nuit",
+		"english": "night",
+		"romaji": ["ya", "yoru/yo"],
+		"hiragana": ["や", "やる・よ"],
+		"kanji": "夜"
+	},
+	{
+		"nederlands": "ochtend",
+		"francais": "matin",
+		"english": "morning",
+		"romaji": ["chou", "asa"],
+		"hiragana": ["ちょう", "あさ"],
+		"kanji": "朝"
+	},
+	{
+		"nederlands": "(vast)houden",
+		"francais": "tenir",
+		"english": "to hold",
+		"romaji": ["ji", "mo"],
+		"hiragana": ["じ", "も"],
+		"kanji": "持"
+	}
+]);
