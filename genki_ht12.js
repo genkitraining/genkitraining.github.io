320b38512b54dfc2cc49ec584d264b5f52b6b290
genkitraining <ballekes@indesoep.be>
2021-05-02 20:44:25 +0200

    Hoofdstuk 12 toegevoegd

---
+++ genki_ht12.js
@@ -0,0 +1,114 @@
+set_state("genki_ht12", [
+	{
+		"nederlands": "ver verleden tijd",
+		"francais": "vieux temps",
+		"english": "ancient times",
+		"romaji": ["mukashi"],
+		"hiragana": ["むかし"],
+		"kanji": "昔"
+	},
+	{
+		"nederlands": "repititiesymbool",
+		"francais": "symbole de repetition",
+		"english": "symbol of repitition of a kanji",
+		"romaji": [""],
+		"hiragana": [""],
+		"kanji": "々"
+	},
+	{
+		"nederlands": "God",
+		"francais": "Dieu",
+		"english": "God",
+		"romaji": ["jin, shin, kou", "kami"],
+		"hiragana": ["じん、しん、こう", "かみ"],
+		"kanji": "神"
+	},
+	{
+		"nederlands": "vroeg",
+		"francais": "tôt",
+		"english": "early",
+		"romaji": ["sou", "haya"],
+		"hiragana": ["そう", "はや"],
+		"kanji": "早"
+	},
+	{
+		"nederlands": "opstaan",
+		"francais": "s'éléver",
+		"english": "to get up",
+		"romaji": ["ki", "o"],
+		"hiragana": ["き", "お"],
+		"kanji": "起"
+	},
+	{
+		"nederlands": "een koe",
+		"francais": "une vache",
+		"english": "cow",
+		"romaji": ["gyuu", "ushi"],
+		"hiragana": ["ぎゅう", "うし"],
+		"kanji": "牛"
+	},
+	{
+		"nederlands": "gebruiken",
+		"francais": "utiliser",
+		"english": "to use",
+		"romaji": ["shi", "tsuka"],
+		"hiragana": ["し", "つか"],
+		"kanji": "使"
+	},
+	{
+		"nederlands": "werken",
+		"francais": "travailler",
+		"english": "to work",
+		"romaji": ["dou", "hatara, batara"],
+		"hiragana": ["どう", "はたら、ばたら"],
+		"kanji": "働"
+	},
+	{
+		"nederlands": "verbinden",
+		"francais": "connecter",
+		"english": "to link",
+		"romaji": ["ren", "tsu"],
+		"hiragana": ["れん", "つ"],
+		"kanji": "連"
+	},
+	{
+		"nederlands": "scheiden",
+		"francais": "séparer",
+		"english": "to separate",
+		"romaji": ["betsu", "waka"],
+		"hiragana": ["べつ", "わか"],
+		"kanji": "別"
+	},
+	{
+		"nederlands": "tijd, graden (temperatuur)",
+		"francais": "temps, degrée",
+		"english": "time, degrees",
+		"romaji": ["do"],
+		"hiragana": ["ど"],
+		"kanji": "度"
+	},
+	{
+		"nederlands": "rood",
+		"francais": "rouge",
+		"english": "red",
+		"romaji": ["seki", "aka"],
+		"hiragana": ["せき", "あか"],
+		"kanji": "赤"
+	},
+	{
+		"nederlands": "blauw",
+		"francais": "bleu",
+		"english": "blue",
+		"romaji": ["sei", "ao"],
+		"hiragana": ["せい", "あお"],
+		"kanji": "青"
+	},
+	{
+		"nederlands": "een kleur",
+		"francais": "une couleur"
+		"english": "colour",
+		"romaji": ["shiki, shoku", "iro"],
+		"hiragana": ["しき、しょく", "いろ"],
+		"kanji": "色"
+	}
+]);
