c60712ee17a769d0557df5ebbb09a30bb503b232
genkitraining <ballekes@indesoep.be>
2021-05-02 20:26:48 +0200

    Hoofdstuk 11 toegevoegd

---
+++ genki_ht11.js
@@ -0,0 +1,130 @@
+set_state("genki_ht11", [
+	{
+		"nederlands": "hand",
+		"francais": "main",
+		"english": "hand",
+		"romaji": ["shu", "te"],
+		"hiragana": ["しゅ", "て"],
+		"kanji": "手"
+	},
+	{
+		"nederlands": "papier",
+		"francais": "papier",
+		"english": "paper",
+		"romaji": ["shi", "gami/kami"],
+		"hiragana": ["し", "がみ、かみ"],
+		"kanji": "紙"
+	},
+	{
+		"nederlands": "favoriet; graag hebben",
+		"francais": "favorit, aimer",
+		"english": "favorite, to like",
+		"romaji": ["kou", "su, kono"],
+		"hiragana": ["こう", "す、この"],
+		"kanji": "好"
+	},
+	{
+		"nederlands": "dichtbij",
+		"francais": "proche",
+		"english": "near",
+		"romaji": ["kin", "chika"],
+		"hiragana": ["きん", "ちか"],
+		"kanji": "近"
+	},
+	{
+		"nederlands": "helder",
+		"francais": "brillant",
+		"english": "bright",
+		"romaji": ["mei", "aka"],
+		"hiragana": ["めい", "あか"],
+		"kanji": "明"
+	},
+	{
+		"nederlands": "ziek",
+		"francais": "malade",
+		"english": "ill, sick",
+		"romaji": ["byou"],
+		"hiragana": ["びょう"],
+		"kanji": "病"
+	},
+	{
+		"nederlands": "instituut",
+		"francais": "institut",
+		"english": "institution", 
+		"romaji": ["in"],
+		"hiragana": ["いん"],
+		"kanji": "院"
+	},
+	{
+		"nederlands": "reflecteren",
+		"francais": "reflecter",
+		"english": "to reflect",
+		"romaji": ["ei", "utsu"],
+		"hiragana": ["えい", "うつ"],
+		"kanji": "映"
+	},
+	{
+		"nederlands": "afbeelding",
+		"francais": "image",
+		"english": "picture",
+		"romaji": ["ga", "kaku"],
+		"hiragana": ["が", "かく"],
+		"kanji": "画"
+	},
+	{
+		"nederlands": "zingen",
+		"francais": "chanter",
+		"english": "to sing",
+		"romaji": ["ka", "uta"],
+		"hiragana": ["か", "うた"],
+		"kanji": "歌"
+	},
+	{
+		"nederlands": "stad",
+		"francais": "ville",
+		"english": "city",
+		"romaji": ["shi", "ichi"],
+		"hiragana": ["し", "いち"],
+		"kanji": "市"
+	},
+	{
+		"nederlands": "plaats",
+		"francais": "lieu",
+		"english": "place",
+		"romaji": ["jo, sho", "tokoro, dokoro"],
+		"hiragana": ["じょ、しょ", "ところ、どころ"],
+		"kanji": "所"
+	},
+	{
+		"nederlands": "inspanning leveren",
+		"francais": "faire des efforts",
+		"english": "to make efforts",
+		"romaji": ["ben", "tsuto"],
+		"hiragana": ["べん", "つと"],
+		"kanji": "勉"
+	},
+	{
+		"nederlands": "sterk",
+		"francais": "fort",
+		"english": "strong",
+		"romaji": ["kyou, gou", "tsuyo"],
+		"hiragana": ["きょう、ごう", "つよ"],
+		"kanji": "強"
+	},
+	{
+		"nederlands": "bestaan",
+		"francais": "exister",
+		"english": "to exist",
+		"romaji": ["yuu", "a"],
+		"hiragana": ["ゆう", "あ"],
+		"kanji": "有"
+	},
+	{
+		"nederlands": "reizen",
+		"francais": "voyager",
+		"english": "travel",
+		"romaji": ["ryo", "tabi"],
+		"hiragana": ["りょ", "たび"],
+		"kanji": "旅"
+	}
+]);
