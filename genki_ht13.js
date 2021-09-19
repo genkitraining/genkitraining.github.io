166c59dcd8ce1739f47daa1968a8a2299e414b67
Genki Training <68614177+genkitraining@users.noreply.github.com>
2021-09-19 16:01:21 +0200

    Create genki_ht13.js

---
+++ genki_ht13.js
@@ -0,0 +1,130 @@
+set_state("genki_ht13", [
+	{
+		"nederlands": "een ding",
+		"francais": "un chose",
+		"english": "thing",
+		"romaji": ["butsu", "mono"],
+		"hiragana": ["ぶつ", "もの"],
+		"kanji": "物"
+	},
+	{
+		"nederlands": "een vogel",
+		"francais": "une oiseau",
+		"english": "bird",
+		"romaji": ["chou"; "tori"],
+		"hiragana": ["ちょう", "とり"],
+		"kanji": "鳥"
+	},
+	{
+		"nederlands": "ingredienten, vergoeding",
+		"francais": "ingredients, salaire",
+		"english": "ingredients, fare",
+		"romaji": ["ryou"],
+		"hiragana": ["りょう"],
+		"kanji": "料"
+	},
+	{
+		"nederlands": "reden",
+		"francais": "raison",
+		"english": "reason",
+		"romaji": ["ri"],
+		"hiragana": ["り"],
+		"kanji": "理"
+	},
+	{
+		"nederlands": "speciaal",
+		"francais": "speciale",
+		"english": "special",
+		"romaji": ["toku", "to_"],
+		"hiragana": ["とく", "とっ"],
+		"kanji": "特"
+	},
+	{
+		"nederlands": "goedkoop, makkelijk",
+		"francais": "bon marché",
+		"english": "cheap, ease",
+		"romaji": ["an", "yasu"],
+		"hiragana": ["あん", "やす"],
+		"kanji": "安"
+	},
+	{
+		"nederlands": "eten, gekookte rijst",
+		"francais": "nouriture, riz",
+		"english": "food, cooked rice",
+		"romaji": ["han"],
+		"hiragana": ["はん"],
+		"kanji": "飯"
+	},
+	{
+		"nederlands": "vlees",
+		"francais": "viande",
+		"english": "meat",
+		"romaji": ["niku"],
+		"hiragana": ["にく"],
+		"kanji": "肉"
+	},
+	{
+		"nederlands": "slecht, fout",
+		"francais": "mal",
+		"english": "bad, wrong",
+		"romaji": ["aku", "waru"],
+		"hiragana": ["あく", "わる"],
+		"kanji": "悪"
+	},
+	{
+		"nederlands": "lichaam",
+		"francais": "corps",
+		"english": "body",
+		"romaji": ["tai", "karada"],
+		"hiragana": ["たい", "からだ"],
+		"kanji": "体"
+	},
+	{
+		"nederlands": "hetzelfde",
+		"francais": "le même",
+		"english": "same",
+		"romaji": ["dou", "ona"],
+		"hiragana": ["どう", "おな"],
+		"kanji": "同"
+	},
+	{
+		"nederlands": "(kledij) dragen, rijken",
+		"francais": "porter",
+		"english": "to reach, to wear",
+		"romaji": ["chaku", "tsu/ki/gi"],
+		"hiragana": ["ちゃく", "つ　き　ぎ"],
+		"kanji": "着"
+	},
+	{
+		"nederlands": "de lucht, leeg(heid)",
+		"francais": "ciel, vide",
+		"english": "sky, empty",
+		"romaji": ["kuu", "sora, a, kara"],
+		"hiragana": ["くう", "そら　あ　から"],
+		"kanji": "空"
+	},
+	{
+		"nederlands": "haven",
+		"francais": "port",
+		"english": "port, harbor",
+		"romaji": ["kou", "minato"],
+		"hiragana": ["こう", "みなと"],
+		"kanji": "港"
+	},
+	{
+		"nederlands": "middag, overdag",
+		"francais": "midi",
+		"english": "noon, daytime",
+		"romaji": ["chuu", "hiru"],
+		"hiragana": ["ちゅう", "ひる"],
+		"kanji": "昼"
+	},
+	{
+		"nederlands": "de zee",
+		"francais": "le mêr",
+		"english": "sea",
+		"romaji": ["kai", "umi"],
+		"hiragana": ["かい", "うみ"],
+		"kanji": "海"
+	}
+]);
