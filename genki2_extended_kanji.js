8b73de0ba7351cc4247b4ab346cde271b76d3a97
genkitraining <ballekes@indesoep.be>
2021-11-09 17:06:52 +0100

    Adding an extended genki2 kanji list

---
+++ genki2_extended_kanji.js
@@ -0,0 +1,17 @@
+var genki2_extended_kanji = [
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	["勿", "物", "鳥", "斗", "料", "里", "理", "特", "安", "反", "飯", "肉", "亜", "悪", "体", "同", "羊", "着", "空", "共", "洪", "港", "旦", "尺", "昼", "氵", "海"],
+	["彳", "皮", "彼", "式", "代", "留", "族", "親", "切", "花", "英", "占", "店", "去", "急", "乗", "当", "音", "楽", "医", "者"]
+];
