df21e02dbc21a1b3f46871a2d75e47d89ba73ae3
genkitraining <ballekes@indesoep.be>
2021-11-09 11:18:48 +0100

    fixing chapter counter for genki 2

--- genki2_kanji.js
+++ genki2_kanji.js
@@ -1,5 +1,17 @@
 var genki2_kanji = [
 	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
 	["物", "鳥", "料", "理", "特", "安", "飯", "肉", "悪", "体", "同", "着", "空", "港", "昼", "海"],
 	["彼", "代", "留", "族", "親", "切", "英", "店", "去", "急", "乗", "当", "音", "楽", "医", "者"]
 ];
